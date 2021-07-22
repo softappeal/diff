a4c749d984b69f48d686fc22e790daf58197bffb
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2021-07-22 12:08:47 +0200

    yass2 8.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,14 @@ import org.jetbrains.kotlin.gradle.tasks.*
 
 defaultTasks("clean", "build", "installDist")
 
-version = "2.0.7"
+version = "2.0.8"
 
 plugins {
-    kotlin("jvm") version "1.5.20"
+    kotlin("jvm") version "1.5.21"
     application
 }
-val coroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0"
-fun yass2(module: String) = "ch.softappeal.yass2:yass2-$module:8.0.0"
+val coroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1"
+fun yass2(module: String) = "ch.softappeal.yass2:yass2-$module:8.0.1"
 
 dependencies {
     implementation(yass2("core"))
