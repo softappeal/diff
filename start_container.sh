a793ae852aa16de9d16f2b3060654bd13afddb6c
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2023-03-20 16:23:10 +0100

    docker added

---
+++ start_container.sh
@@ -0,0 +1,7 @@
+docker container rm --force project-container
+docker image     rm --force project-image
+
+docker image     build                                               --tag project-image .
+docker container create --name project-container --tty --interactive       project-image /bin/bash
+
+docker container start --attach --interactive project-container
