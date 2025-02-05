51edb080f44e56ff221319a9e06f020a9e77bad0
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2025-02-05 15:48:11 +0100

    yass2 20.0.0

--- src/main/kotlin/GeneratedByYass.kt
+++ src/main/kotlin/GeneratedByYass.kt
@@ -10,10 +10,24 @@
     "KotlinRedundantDiagnosticSuppress",
     "RedundantSuppression",
     "UNUSED_ANONYMOUS_PARAMETER",
+    "TrailingComma",
 )
 
 package ch.softappeal.diff
 
+/*
+    1: kotlin.collections.List
+    2: kotlin.String
+    3: kotlin.Int
+    4: kotlin.ByteArray
+    5: ch.softappeal.diff.FileNode
+        name: required 2
+        digest: required 4
+        size: required 3
+    6: ch.softappeal.diff.DirectoryNode
+        name: required 2
+        nodes: required 1
+*/
 public fun createBinarySerializer(): ch.softappeal.yass2.serialize.binary.BinarySerializer =
     object : ch.softappeal.yass2.serialize.binary.BinarySerializer() {
         init {
@@ -24,29 +38,29 @@ public fun createBinarySerializer(): ch.softappeal.yass2.serialize.binary.Binary
                 ch.softappeal.yass2.serialize.binary.BinaryEncoder(
                     ch.softappeal.diff.FileNode::class,
                     { i ->
-                        writeNoIdRequired(2, i.name)
-                        writeNoIdRequired(4, i.digest)
-                        writeNoIdRequired(3, i.size)
+                        writeRequired(i.name, 2)
+                        writeRequired(i.digest, 4)
+                        writeRequired(i.size, 3)
                     },
                     {
                         val i = ch.softappeal.diff.FileNode(
-                            readNoIdRequired(2) as kotlin.String,
+                            readRequired(2) as kotlin.String,
                         )
-                        i.digest = readNoIdRequired(4) as kotlin.ByteArray
-                        i.size = readNoIdRequired(3) as kotlin.Int
+                        i.digest = readRequired(4) as kotlin.ByteArray
+                        i.size = readRequired(3) as kotlin.Int
                         i
                     }
                 ),
                 ch.softappeal.yass2.serialize.binary.BinaryEncoder(
                     ch.softappeal.diff.DirectoryNode::class,
                     { i ->
-                        writeNoIdRequired(2, i.name)
-                        writeNoIdRequired(1, i.nodes)
+                        writeRequired(i.name, 2)
+                        writeRequired(i.nodes, 1)
                     },
                     {
                         val i = ch.softappeal.diff.DirectoryNode(
-                            readNoIdRequired(2) as kotlin.String,
-                            readNoIdRequired(1) as kotlin.collections.List<ch.softappeal.diff.Node>,
+                            readRequired(2) as kotlin.String,
+                            readRequired(1) as kotlin.collections.List<ch.softappeal.diff.Node>,
                         )
                         i
                     }
